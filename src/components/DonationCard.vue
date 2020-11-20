d9d414488b17916b34534fa7c5a51ce0bf68bb23
LunaticHacker <lunatechnology007@gmail.com>
2020-11-20 11:16:15 -0600

    donation cards

---
+++ src/components/DonationCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-card">
+      <p class="amount">{{amount}}</p>
+      <p>{{description}}</p>
+      </div>
+</template>
+
+<script>
+export default {
+  props: ['amount', 'description'],
+};
+</script>>
+
+<style scoped>
+.d-card
+{
+    color: white;
+    background-color: forestgreen;
+    width:10em;
+    border-radius: 10%;
+    margin: 1em;
+    padding: 1em;
+}
+.amount
+{
+    font-size: 2em;
+}
+.amount::before
+{
+    content:"\20B9";
+}
+
+</style>
--- src/views/Donate.vue
+++ src/views/Donate.vue
@@ -6,11 +6,30 @@
       </div>
       <div>
           <h1>Help us work for a Better Future</h1>
-          <!-- Place for Cards Explaining Impact of Donation -->
+          <div class="card-area">
+          <DonateCard amount="100"
+          description="A brief description of how we will utilize this amount"/>
+         <DonateCard amount="500"
+          description="A brief description of how we will utilize this amount"/>
+         <DonateCard amount="1000"
+          description="A brief description of how we will utilize this amount"/>
+          <DonateCard amount="5000"
+          description="A brief description of how we will utilize this amount"/>
+          </div>
       </div>
   </div>
   <footer><a href="http://www.freepik.com">All Illustrations are Designed by pch.vector / Freepik</a></footer>
 </template>
+
+<script>
+import DonateCard from '@/components/DonationCard.vue';
+
+export default {
+  components: {
+    DonateCard,
+  },
+};
+</script>
 
 <style>
 .donate
@@ -30,6 +49,12 @@
         width: 100vw;
         height: 90vh;
     }
+}
+.card-area
+{
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
 }
 
 </style>
