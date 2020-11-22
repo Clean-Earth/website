c37c989efa6b3275f74e394c852649cfed820cce
LunaticHacker <lunatechnology007@gmail.com>
2020-11-22 03:24:28 -0600

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/fancy-borders.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub, faTelegram, faYoutube, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faGithub, faYoutube, faTelegram, faWhatsapp);
 
-createApp(App)
-  .use(router)
-  .mount("#app");
+const app = createApp(App);
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
