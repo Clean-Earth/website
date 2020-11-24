976f7c168e7da749171d3ced7491bdbf4fb069aa
LunaticHacker <lunatechnology007@gmail.com>
2020-11-24 03:53:19 -0600

    Add some content for learn page

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,32 @@
 /* eslint-disable */
 const learndata = [
-  { title: "Buy Less", desc: "Description", class: "cu" },
-  { title: "Repair and Reuse", desc: "Description", class: "cb" },
-  { title: "Avoid Plastic", desc: "Description", class: "bc" },
-  { title: "Composit food waste", desc: "Description", class: "sb" },
-  { title: "Recycle", desc: "Description", class: "bc" },
-  { title: "Use Electronic Media", desc: "Description", class: "cu" },
-  { title: "Plan Your Meals", desc: "Description", class: "cb" },
-  { title: "Tell your Politicians", desc: "Description", class: "sb" }
+  { title: "Repair and Reuse", desc: "Repair your belongings rather than buying new", class: "cu" },
+  { title: "Avoid Plastic", desc: "Avoid using plastic and switch to alternatives", class: "bc" },
+  {
+    title: "Composit",
+    desc: "Composit food waste in your backyard",
+    class: "sb"
+  },
+  { title: "ANother THing", desc: "Description", class: "cu" },
+  {
+    title: "Recycle",
+    desc: "Buy items with no/less packaging and Recycle it whenever possible ",
+    class: "bc"
+  },
+  {
+    title: "Stop using Paper",
+    desc: "Switch to Digital Media",
+    class: "cu"
+  },
+  {
+    title: "Cut food waste",
+    desc: "Reduce the amount of food that is wasted or thrown away in your home",
+    class: "cb"
+  },
+  {
+    title: "Tell your Politicians",
+    desc: "Ask politicians to act or invest to support our steps",
+    class: "sb"
+  }
 ];
 export default { learndata };
