903f2bce57b7797e0cb71ae9e35a245e6705bbf9
LunaticHacker <lunatechnology007@gmail.com>
2020-11-24 06:22:24 -0600

    first pass on event section design

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,14 +8,40 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
+      <h1>Upcoming Events</h1>
+      <div class="event-section">
+      <EventCard/>
+      <EventCard/>
+      </div>
+      <h1>Past Activities</h1>
+      <div class="event-section">
+      <EventCard/>
+      <EventCard/>
+      <EventCard/>
+      </div>
   </div>
    <footer><a href="http://www.freepik.com">All Illustrations are Designed by pch.vector / Freepik</a></footer>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+
+export default {
+  components: { EventCard },
+};
+</script>>
+
 <style>
 .abt-image
 {
   width: 700px;
   height: 450px;
+}
+.event-section
+{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 @media screen and (max-width:768px)
 {
